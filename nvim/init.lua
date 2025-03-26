3b7273ee23db8d55e77e8429401c09a85b60b0f9
KerndBloOs <bloosmin@gamebox>
2025-03-27 00:01:02 +0100

    add ansible lsp settings

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,18 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- Setup LSP
+local lspconfig = require("lspconfig")
+
+-- Install Ansible Language Server (requires ansible-lint & ansible)
+lspconfig.ansiblels.setup({
+  cmd = { "ansible-language-server", "--stdio" },
+  filetypes = { "yaml", "yml" },
+  settings = {
+    ansible = {
+      ansibleLint = {
+        enabled = true,
+      },
+    },
+  },
+})
