a16de6a51edfe71c70e1bab039fe0ce80be1bea7
kerndbloos <bloosmin@gamebox>
2025-05-05 15:03:12 +0200

    remove telescope keybindings

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -1,50 +1,41 @@
 return {
-  {
-    "nvim-telescope/telescope.nvim",
-    keys = {
-      { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files Telescope" },
-    },
-    opts = {
-      pickers = {
-        find_files = {
-          -- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
-          find_command = { "rg", "--files", "--glob", "!**/.git/*", "-L" },
-        },
-      },
-    },
-    defaults = {
-      path_display = { "smart" },
-      mappings = {
-        i = {
-          ["<C-k>"] = actions.move_selection_previous, -- move to prev result
-          ["<C-j>"] = actions.move_selection_next,     -- move to next result
-        },
-      },
-    },
-  },
-  {
-    "telescope.nvim",
-    dependencies = {
-      "nvim-telescope/telescope-fzf-native.nvim",
-      build = "make",
-      config = function()
-        require("telescope").load_extension("fzf")
-      end,
-    },
-  },
-  {
-    "nvim-telescope/telescope-symbols.nvim",
-  },
-  -- Custom ripgrep configuration:
+	{
+		"nvim-telescope/telescope.nvim",
+		keys = {
+			{ "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files Telescope" },
+		},
+		opts = {
+			pickers = {
+				find_files = {
+					-- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
+					find_command = { "rg", "--files", "--glob", "!**/.git/*", "-L" },
+				},
+			},
+		},
+	},
+	{
+		"telescope.nvim",
+		dependencies = {
+			"nvim-telescope/telescope-fzf-native.nvim",
+			build = "make",
+			config = function()
+				require("telescope").load_extension("fzf")
+			end,
+		},
+	},
+	{
+		"nvim-telescope/telescope-symbols.nvim",
+	},
+	-- Custom ripgrep configuration:
 
-  -- I want to search in hidden/dot files.
-  -- "--hidden"
-  --
-  -- I don't want to search in the `.git` directory.
-  --j"--glob")
-  --j"!**/.git/*")
-  --
-  --  I want to follow symbolic links
-  -- "-L"
-  --
+	-- I want to search in hidden/dot files.
+	-- "--hidden"
+	--
+	-- I don't want to search in the `.git` directory.
+	--j"--glob")
+	--j"!**/.git/*")
+	--
+	--  I want to follow symbolic links
+	-- "-L"
+	--
 }
