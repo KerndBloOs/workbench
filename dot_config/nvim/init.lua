dd04003759b7f03893f700ea6664fac494794594
kerndbloos <bloosmin@gamebox>
2025-05-08 19:49:13 +0200

    delete ansible lspconfig

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,17 +1,1 @@
 require("config.lazy")
-
--- Setup LSP
-local lspconfig = require("lspconfig")
-
--- Install Ansible Language Server (requires ansible-lint & ansible)
-lspconfig.ansiblels.setup({
-  cmd = { "ansible-language-server", "--stdio" },
-  filetypes = { "yaml", "yml" },
-  settings = {
-    ansible = {
-      ansibleLint = {
-        enabled = true,
-      },
-    },
-  },
-})
