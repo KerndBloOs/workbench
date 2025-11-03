5d6e7d5a1bd98e05c82c2440dd13564917934c24
kerndbloos <vandante@gmx.de>
2025-11-03 21:16:59 +0100

    change color theme to kanagawa dragon

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -8,17 +8,56 @@
 --    },
 --  },
 --}
+--return {
+--	-- Kanagawa colorscheme
+--	{
+--		"rebelot/kanagawa.nvim",
+--		lazy = false,
+--		priority = 1000,
+--		config = function()
+--			---@diagnostic disable-next-line: missing-fields
+--			require("kanagawa").setup({
+--				transparent = false, -- keep normal background, only target line numbers
+--				theme = "dragon",
+--				colors = {
+--					theme = {
+--						all = {
+--							ui = {
+--								bg_gutter = "none",
+--							},
+--						},
+--					},
+--				},
+--				overrides = function(colors)
+--					return {
+--						LineNr = { bg = "NONE" },
+--						LineNrAbove = { bg = "NONE" },
+--						LineNrBelow = { bg = "NONE" },
+--						CursorLineNr = { bg = "NONE" },
+--					}
+--				end,
+--			})
+--			vim.cmd.colorscheme("kanagawa")
+--		end,
+--	},
+--
+--	-- LazyVim configuration
+--	{
+--		"LazyVim/LazyVim",
+--		opts = {
+--			colorscheme = "kanagawa",
+--		},
+--	},
+--}
 return {
-	-- Kanagawa colorscheme
 	{
 		"rebelot/kanagawa.nvim",
 		lazy = false,
 		priority = 1000,
 		config = function()
-			---@diagnostic disable-next-line: missing-fields
 			require("kanagawa").setup({
-				transparent = false, -- keep normal background, only target line numbers
 				theme = "dragon",
+				-- other options as above
 				colors = {
 					theme = {
 						all = {
@@ -28,24 +67,9 @@ return {
 						},
 					},
 				},
-				overrides = function(colors)
-					return {
-						LineNr = { bg = "NONE" },
-						LineNrAbove = { bg = "NONE" },
-						LineNrBelow = { bg = "NONE" },
-						CursorLineNr = { bg = "NONE" },
-					}
-				end,
 			})
-			vim.cmd.colorscheme("kanagawa")
+			vim.cmd("colorscheme kanagawa-dragon")
 		end,
 	},
-
-	-- LazyVim configuration
-	{
-		"LazyVim/LazyVim",
-		opts = {
-			colorscheme = "kanagawa",
-		},
-	},
+	-- other plugins
 }
